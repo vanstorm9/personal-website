b02bc270b0eee5cefa0fbb1d10db635611f90855
vanstorm9 <antlowhur@yahoo.com>
2016-07-04 19:26:31 -0500

    I think I finished the alpha version

---
+++ other-sites/llsif/audio-handle.js
@@ -0,0 +1,88 @@
+var globalAudio = null;
+
+	function commandSelect(mode)
+	{
+		// mode == 0 is home button
+		// mode == 1 is waifu button
+		// mode == 2 is story button
+		// mode == 3 is member button
+		if (globalAudio!=null){
+			globalAudio.pause();
+		}
+		
+
+		var audioPath = "audio/";
+		var waifuName = "hanayo/";
+		var file;
+
+
+		// Number between 0 and maxNum
+		
+
+		var n = 0;
+
+		if(mode == 0){
+			// Home button RNG
+			var maxNum = 15;
+			n = Math.floor(Math.random() * maxNum);
+			file = "home/";
+		} else if (mode == 1){
+			// Waifu button RNG
+			var maxNum = 11;
+			n = Math.floor(Math.random() * maxNum);
+			file = "waifu/";
+		} else if (mode == 2){
+			// Story button RNG
+			var maxNum = 1;
+			n = Math.floor(Math.random() * maxNum);
+			file = "story/";
+		} else if (mode == 3){
+			// Member button RNG
+			var maxNum = 1;
+			n = Math.floor(Math.random() * maxNum);
+			file = "member/";
+		}
+
+
+
+		var superString = "".concat(audioPath, waifuName, file, n, ".mp3");
+		globalAudio = new Audio(superString);
+		
+		globalAudio.play();
+
+	}
+
+	function homeClick() {
+    	var x = document.getElementById("homeScreen").useMap = "#clickmap";
+
+		var mode = 0;
+		commandSelect(mode);
+
+	}
+
+
+	function waifuClick()
+	{
+		
+		var x = document.getElementById("homeScreen").useMap = "#clickmap";
+
+		var mode = 1;
+		commandSelect(mode);
+	}
+
+	function storyClick()
+	{
+
+		var x = document.getElementById("homeScreen").useMap = "#clickmap";
+
+		var mode = 2;
+		commandSelect(mode);
+	}
+
+	function memberClick()
+	{
+		var x = document.getElementById("homeScreen").useMap = "#clickmap";
+
+		var mode = 3;
+		commandSelect(mode);
+	}
